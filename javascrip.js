4df52afb8c947904a555cdd819370d4a5d118040
Ivana <79462114+IvanaWinc@users.noreply.github.com>
2021-08-11 16:49:58 +0200

    Add files via upload

---
+++ javascrip.js
@@ -0,0 +1,12 @@
+var age = 22;
+
+var Female = true;
+var Male = false;
+
+var driverStatus = ("Bob");
+
+if (age < 18) {
+    console.log("You are not old enough");
+} else (age > 18, driverStatus); {
+    console.log("You are old enough, welcome Bob");
+}
